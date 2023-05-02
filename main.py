9310db396155a1a940336862a131cfca8daf63b4
yasmercy1337 <ianchen4231@gmail.com>
2023-05-02 15:05:24 -0700

    WORKIGN GRADIENT DESCENT FOR ONE DATA POINT

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
-from network import Network
+from network import *
 import numpy as np
+import random
+
+
+def create_dataset(n: int) -> tuple[np.ndarray, np.ndarray]:
+    """ Creates the inputs and expected outputs"""
+    data = list(range(n * 3))
+    inputs = np.array(random.sample(data, n))
+    outputs = np.array(inputs, copy=True)
+
+    return inputs, outputs
+
+def train_test_split(arr: np.ndarray, n: float = 0.5) -> tuple[np.ndarray, np.ndarray]:
+    """ Splits the given dataset into a training and testing """
+    cutoff = int(len(arr) * n)
+    return arr[:cutoff], arr[cutoff:]
 
 def main():
-    net = Network(1, 1, 0, 1)
-    print(net)
-    
-    out = net.evaluate(np.array([2]))
-    print(net.cost(np.array([2]), np.array([2])))
+    net = Network(1, 1, 3, 3)
+
+    inputs, outputs = create_dataset(10 ** 6)
+
+    for _ in range(10):
+        out = net.evaluate(np.array([2]))
+        real = np.array([2])
+        dW, dB = net.gradient(inputs, real)
+        for w, b, layer in zip(dW, dB, net.layers):
+            layer.weights -= w * 0.01
+            layer.biases -= b * 0.01
+
+
+        print(out, real, cost(out, real))
+        # print(net, "\n")
 
 if __name__ == "__main__":
     main()
--- network.py
+++ network.py
@@ -7,15 +7,13 @@ class Layer:
     def __init__(self, num_inputs: int, num_outputs: int):
         """ Initializes a matrix of weights and biases """
         self.weights = np.random.rand(num_outputs, num_inputs)
-        self.biases = np.random.rand(num_outputs, 1)
-    
-    def activate_output(self, output: float) -> float:
-        """ Calls an activation function (ReLU) on an output """
-        return max(0, output)
+        self.biases = np.random.rand(num_outputs)
+        self.last_activated = np.random.rand(num_outputs)
     
     def evaluate(self, inputs: np.ndarray) -> np.ndarray:
         """ Calls an activation function on the outputs of the layer """
-        return self.activate_output(self.weights @ inputs + self.biases)
+        self.last_activated = np.array([activate(x) for x in (self.weights @ inputs + self.biases)])
+        return self.last_activated
     
     def __repr__(self) -> str:
         """ Returns a string representation of this layer """
@@ -24,8 +22,8 @@ class Layer:
 class Network:
     def __init__(self, num_inputs: int, num_outputs: int, num_hidden_layers: int, layer_size: int) -> None:
         """ Initializes an array of layers """
-        dimensions = [num_inputs] + [layer_size] * num_hidden_layers + [num_outputs]
-        self.layers = [Layer(inputs, outputs) for (inputs, outputs) in it.pairwise(dimensions)]
+        self.dimensions = [num_inputs] + [layer_size] * num_hidden_layers + [num_outputs]
+        self.layers = [Layer(inputs, outputs) for (inputs, outputs) in it.pairwise(self.dimensions)]
         
     def evaluate(self, inputs: np.ndarray) -> np.ndarray:
         """ Feeds the input through each layer of this network """
@@ -37,13 +35,48 @@ class Network:
         """ Returns a string representation of this network """
         return "\n\n".join(map(repr, self.layers))
     
-    def cost(self, inputs: np.ndarray, real: np.ndarray) -> float:
-        """ Mean squared error between real outputs and generated outputs """
-        squared_errors = [(x - y) ** 2 for (x, y) in zip(real, self.evaluate(inputs))]
-        return sum(squared_errors) / len(squared_errors)
-        # NOTE: in practice, this is the mean error for a whole training set, not just a data point
-    
-    def gradient(self, inputs: np.ndarray, real: np.ndarray) -> np.ndarray:
-        """ Computes the gradient for the current neural network (given a data point)"""
-        # NOTE: in real gradient descent, this is done for all data points and averaged out
-        
+    def gradient(self, inputs: np.ndarray, real: np.ndarray, learning_rate: float = 0.01
+                 ) -> tuple[list[np.ndarray], list[np.ndarray]]:
+        """ Computes the gradient and adjusts the weights and biases accordingly """
+        d_biases = [np.zeros(self.dimensions[layer_num + 1]) for layer_num in range(len(self.layers))]
+        d_weights = [np.zeros((self.dimensions[layer_num + 1], self.dimensions[layer_num])) 
+                     for layer_num in range(len(self.layers))]
+
+        expected = self.layers[-1].last_activated
+        dA = np.array([2 * (a - y) for (a, y) in zip(expected, real)])
+        activations = [inputs] + [self.layers[layer_num].last_activated for layer_num in range(len(self.layers))] + [expected]
+        for layer_num in reversed(range(len(self.layers))):
+            dB = np.zeros(self.dimensions[layer_num + 1])
+            dW = np.zeros((self.dimensions[layer_num + 1], self.dimensions[layer_num]))
+            for j in range(self.dimensions[layer_num + 1]): # 2
+                dA_dZ = activate_derivative(activations[layer_num + 1][j])
+                dB[j] = dA[j] * dA_dZ
+                for k in range(self.dimensions[layer_num]): # 3
+                    dW[j, k] = dB[j] * activations[layer_num][k]
+            
+            d_biases[layer_num] += dB
+            d_weights[layer_num] += dW
+
+            if layer_num == 0:
+                break
+            dA_new = np.zeros(self.dimensions[layer_num])
+            for j in range(self.dimensions[layer_num + 1]):
+                dA_dZ = activate_derivative(activations[layer_num + 1][j])
+                for k in range(self.dimensions[layer_num]):
+                    dA_new[k] += dA[j] * dA_dZ * self.layers[layer_num].weights[j, k]
+            dA = dA_new
+        return d_weights, d_biases
+
+
+def activate(x: float) -> float:
+    """ Calls an activation function (ReLU) """
+    return max(0, x)
+
+def activate_derivative(x: float) -> float:
+    """ Returns 0 if x < 0, else 1 """
+    return float(x > 0)
+
+def cost(expected: np.ndarray, real: np.ndarray) -> float:
+    """ Mean squared error between real outputs and generated outputs """
+    squared_errors = [(x - y) ** 2 for (x, y) in zip(real, expected)]
+    return sum(squared_errors) / len(squared_errors)
