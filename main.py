6a2c895a0ed979bcc8dc669c3ab8df178752871e
yasmercy1337 <ianchen4231@gmail.com>
2023-05-02 17:11:48 -0700

    training for identity

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ import random
 def create_dataset(n: int) -> tuple[np.ndarray, np.ndarray]:
     """ Creates the inputs and expected outputs"""
     data = list(range(n * 3))
-    inputs = np.array(random.sample(data, n))
+    chosen = random.sample(data, n)
+
+    inputs = np.array([np.array([x]) for x in chosen])
     outputs = np.array(inputs, copy=True)
 
     return inputs, outputs
@@ -18,20 +20,23 @@ def train_test_split(arr: np.ndarray, n: float = 0.5) -> tuple[np.ndarray, np.nd
 
 def main():
     net = Network(1, 1, 3, 3)
+    # print(net)
+    inputs, outputs = create_dataset(10)
+    train_in, train_out = train_test_split(inputs)
+    test_in, test_out = train_test_split(inputs)
 
-    inputs, outputs = create_dataset(10 ** 6)
+    # for _ in range(10):
+    #     out = net.evaluate(np.array([2]))
+    #     real = np.array([2])
+    #     net.update(net.gradient(np.array([2]), real))
 
-    for _ in range(10):
-        out = net.evaluate(np.array([2]))
-        real = np.array([2])
-        dW, dB = net.gradient(inputs, real)
-        for w, b, layer in zip(dW, dB, net.layers):
-            layer.weights -= w * 0.01
-            layer.biases -= b * 0.01
-
-
-        print(out, real, cost(out, real))
+    #     print(out, real, cost(out, real))
         # print(net, "\n")
+
+    net.train(train_in, train_out, iterations=10000)
+    # print(net)
+    print(net.evaluate(np.array([5])))
+    print(net.evaluate(np.array([10])))
 
 if __name__ == "__main__":
     main()
--- network.py
+++ network.py
@@ -2,6 +2,8 @@ import numpy as np
 import itertools as it
 import functools as ft
 
+Gradient = tuple[list[np.ndarray], list[np.ndarray]]
+
 class Layer:
     """ A matrix of weights and biases """
     def __init__(self, num_inputs: int, num_outputs: int):
@@ -35,23 +37,26 @@ class Network:
         """ Returns a string representation of this network """
         return "\n\n".join(map(repr, self.layers))
     
-    def gradient(self, inputs: np.ndarray, real: np.ndarray, learning_rate: float = 0.01
-                 ) -> tuple[list[np.ndarray], list[np.ndarray]]:
-        """ Computes the gradient and adjusts the weights and biases accordingly """
+    def zero_gradient(self) -> Gradient:
+        """ Returns a gradient (weights, biases) with only zeros """
         d_biases = [np.zeros(self.dimensions[layer_num + 1]) for layer_num in range(len(self.layers))]
         d_weights = [np.zeros((self.dimensions[layer_num + 1], self.dimensions[layer_num])) 
                      for layer_num in range(len(self.layers))]
-
+        return d_weights, d_biases
+    
+    def gradient(self, inputs: np.ndarray, real: np.ndarray) -> Gradient:
+        """ Computes the gradient and adjusts the weights and biases accordingly """
+        d_weights, d_biases = self.zero_gradient()
         expected = self.layers[-1].last_activated
         dA = np.array([2 * (a - y) for (a, y) in zip(expected, real)])
         activations = [inputs] + [self.layers[layer_num].last_activated for layer_num in range(len(self.layers))] + [expected]
         for layer_num in reversed(range(len(self.layers))):
             dB = np.zeros(self.dimensions[layer_num + 1])
             dW = np.zeros((self.dimensions[layer_num + 1], self.dimensions[layer_num]))
-            for j in range(self.dimensions[layer_num + 1]): # 2
+            for j in range(self.dimensions[layer_num + 1]):
                 dA_dZ = activate_derivative(activations[layer_num + 1][j])
                 dB[j] = dA[j] * dA_dZ
-                for k in range(self.dimensions[layer_num]): # 3
+                for k in range(self.dimensions[layer_num]):
                     dW[j, k] = dB[j] * activations[layer_num][k]
             
             d_biases[layer_num] += dB
@@ -67,16 +72,40 @@ class Network:
             dA = dA_new
         return d_weights, d_biases
 
+    def update(self, gradient: Gradient, learning_rate: float = 0.01):
+        for w, b, layer in zip(*gradient, self.layers):
+            layer.weights -= w * learning_rate
+            layer.biases -= b * learning_rate
 
+    def train(self, inputs: list[np.ndarray], real_outputs: list[np.ndarray], 
+              iterations: int = 10, learning_rate: float = 0.000001) -> None:
+        """ Takes in labeled dataset and trains for n iterations"""
+        for _ in range(iterations):
+            dW, dB = self.zero_gradient()
+            error = 0
+            for (input, real) in zip(inputs, real_outputs):
+                error += cost(self.evaluate(input), real) / len(inputs)
+                w, b = self.gradient(input, real)
+                dW, dB = v_add(dW, w), v_add(dB, b)
+            self.update((dW, dB), learning_rate / len(inputs))
+            # print(error)
+                
 def activate(x: float) -> float:
     """ Calls an activation function (ReLU) """
-    return max(0, x)
+    # return max(0, x)
+    # return 1 / (1 + 2.71 ** -x)
+    return x
 
 def activate_derivative(x: float) -> float:
     """ Returns 0 if x < 0, else 1 """
-    return float(x > 0)
+    # return float(x > 0)
+    # return 1 / ((2.71 ** x) * (1 + 2.71 ** -x) ** 2)
+    return 1
 
 def cost(expected: np.ndarray, real: np.ndarray) -> float:
     """ Mean squared error between real outputs and generated outputs """
     squared_errors = [(x - y) ** 2 for (x, y) in zip(real, expected)]
     return sum(squared_errors) / len(squared_errors)
+
+def v_add(x: list, y: list) -> list:
+    return [(a + b) for (a, b) in zip(x, y)]
